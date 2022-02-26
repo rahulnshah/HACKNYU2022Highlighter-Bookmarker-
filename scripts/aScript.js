5fa453405e833a3cd79a1699abd6dce49bc8c022
Rahul Shah <rahulnshah3012@gmail.com>
2022-02-26 12:29:21 -0500

    seeting up highlighter is done, now need to work on the save button that is on every div

--- scripts/aScript.js
+++ scripts/aScript.js
@@ -1,3 +1,28 @@
+function showPopup(){      
+  let flash = document.getElementById("myDiv");
+//let flashDiv = document.createElement("div");
+  let outerDiv = document.createElement("div");
+  //outerDiv.className = "row justify-content-center";
+  let innerDiv = document.createElement("div");
+  innerDiv.innerText = "Would you like to save this highlight?\n" + window.getSelection().toString();
+  outerDiv.appendChild(innerDiv);
+  flash.appendChild(outerDiv);
+  let saveButton = document.createElement("button");
+  saveButton.innerHTML = "Save";
+  saveButton.setAttribute('id','saveBtn');
+  let noButton = document.createElement("button");
+  noButton.setAttribute('id','noBtn');
+  noButton.addEventListener("click", function(){
+    this.parentNode.parentNode.removeChild(this.parentNode);
+  });
+  let lineBr = document.createElement("br");
+  noButton.innerHTML = "No";
+  flash.style.backgroundColor = "lightgreen";
+  flash.style.border = "5px outset red";
+  outerDiv.appendChild(saveButton);
+  outerDiv.appendChild(lineBr);
+  outerDiv.appendChild(noButton);
+}
 function myFunction_2()
 {
     //change the back ground color of the selected text 
@@ -5,27 +30,10 @@ function myFunction_2()
     
     if(selObj.toString().length > 0)
     {
-      console.log(selObj.toString());
+      showPopup();
+    }
+    else{
+      alert("need to select text first.");
     }
 }
 document.getElementById("demo").addEventListener("mouseup", myFunction_2);
-document.getElementById("demo").addEventListener("mouseup", function(){      
-	  let flash = document.getElementById("myDiv");
-	//let flashDiv = document.createElement("div");
-	  let outerDiv = document.createElement("div");
-    //outerDiv.className = "row justify-content-center";
-    let innerDiv = document.createElement("div");
-    innerDiv.innerText = "Would you like to save this highlight?\n" + window.getSelection().toString();
-    outerDiv.appendChild(innerDiv);
-    flash.appendChild(outerDiv);
-    let saveButton = document.createElement("button");
-    saveButton.innerHTML = "Save";
-    let noButton = document.createElement("button");
-    let lineBr = document.createElement("br");
-    noButton.innerHTML = "No";
-    flash.style.backgroundColor = "lightgreen";
-    flash.style.border = "5px outset red";
-    flash.appendChild(saveButton);
-    flash.appendChild(lineBr);
-    flash.appendChild(noButton);
-});
