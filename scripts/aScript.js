fe8e7d42ea7c767ddc775b2bf1dd708f071be058
Rahul Shah <rahulnshah3012@gmail.com>
2022-02-26 11:50:18 -0500

    creaed the popup, now need to add functionality to it.

--- scripts/aScript.js
+++ scripts/aScript.js
@@ -9,3 +9,23 @@ function myFunction_2()
     }
 }
 document.getElementById("demo").addEventListener("mouseup", myFunction_2);
+document.getElementById("demo").addEventListener("mouseup", function(){      
+	  let flash = document.getElementById("myDiv");
+	//let flashDiv = document.createElement("div");
+	  let outerDiv = document.createElement("div");
+    //outerDiv.className = "row justify-content-center";
+    let innerDiv = document.createElement("div");
+    innerDiv.innerText = "Would you like to save this highlight?\n" + window.getSelection().toString();
+    outerDiv.appendChild(innerDiv);
+    flash.appendChild(outerDiv);
+    let saveButton = document.createElement("button");
+    saveButton.innerHTML = "Save";
+    let noButton = document.createElement("button");
+    let lineBr = document.createElement("br");
+    noButton.innerHTML = "No";
+    flash.style.backgroundColor = "lightgreen";
+    flash.style.border = "5px outset red";
+    flash.appendChild(saveButton);
+    flash.appendChild(lineBr);
+    flash.appendChild(noButton);
+});
