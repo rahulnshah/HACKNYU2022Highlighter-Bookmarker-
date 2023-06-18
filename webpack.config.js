e5c9433ed63959f4c1e97b3e39df062a3272430a
Rahul Shah <rahulnshah3012@gmail.com>
2023-06-18 19:02:09 -0400

    react-app complete

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
 
 module.exports = {
   entry: {
@@ -20,5 +23,15 @@ module.exports = {
         }
     }
     ]
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template : "./src/popup.html",
+    filename : "popup.html"
+  }),
+    new CopyPlugin({
+      patterns: [
+        { from: "public"}
+      ],
+    }),
+  ],
 };
