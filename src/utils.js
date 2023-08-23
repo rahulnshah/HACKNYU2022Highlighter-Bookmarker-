cadcd08ecbdb71ede5650d266abb1126c05a20fe
Rahul Shah <rahulnshah3012@gmail.com>
2023-08-23 17:21:35 -0400

    fixed UI top highlights don't get cut off

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 export async function getCurrentTab() {
-    let queryOptions = { active: true, lastFocusedWindow: true };
+    let queryOptions = {currentWindow: true, active: true};
     // `tab` will either be a `tabs.Tab` instance or `undefined`.
     let [tab] = await chrome.tabs.query(queryOptions);
     return tab;
